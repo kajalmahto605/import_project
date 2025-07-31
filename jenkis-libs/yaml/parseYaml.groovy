23878acbc4681ea4e8e0c257f67e32c74d72f6cf
kajalmahto605 <kajal.mahato@techanek.com>
2025-07-31 11:48:29 +0530

    Update parseYaml.groovy

--- jenkis-libs/yaml/parseYaml.groovy
+++ jenkis-libs/yaml/parseYaml.groovy
@@ -1,21 +1,11 @@
+@GrabResolver(name='central', root='https://repo1.maven.org/maven2/')
+@Grab('org.yaml:snakeyaml:2.0')
+
 import org.yaml.snakeyaml.Yaml
-import java.net.URLClassLoader
 
-// Add SnakeYAML jar to classpath dynamically
-def jarFile = new File("${WORKSPACE}/jenkis-libs/yaml/snakeyaml-2.0.jar")
-if (jarFile.exists()) {
-    def loader = this.class.classLoader
-    if (loader instanceof groovy.lang.GroovyClassLoader) {
-        loader.addURL(jarFile.toURI().toURL())
-    } else {
-        throw new Exception("Unable to load jar into classloader")
-    }
-} else {
-    throw new FileNotFoundException("Missing snakeyaml jar at: ${jarFile}")
-}
-
-// YAML parser function
 def parseYaml(String yamlText) {
-    def yaml = new org.yaml.snakeyaml.Yaml()
+    def yaml = new Yaml()
     return yaml.load(yamlText)
 }
+
+return this
